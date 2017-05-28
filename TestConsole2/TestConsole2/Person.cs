4b785c559a8130aeafe4c06c770f84a45db92ae7
venkat-giri <giri.ragav@gmail.com>
2017-05-28 14:01:42 -0500

    Created public Class

---
+++ TestConsole2/TestConsole2/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestConsole2
+{
+    partial class Program
+    {
+        public class Person
+        {
+            public string firstName;
+            public string lastName;
+
+            public void Introduce()
+            {
+                Console.WriteLine("Hello! my name is " + firstName + " " + lastName);
+            }
+        }
+    }
+}
--- TestConsole2/TestConsole2/Program.cs
+++ TestConsole2/TestConsole2/Program.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestConsole2
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
@@ -16,8 +15,11 @@ namespace TestConsole2
             var s = "1234";
             int j = int.Parse(s);
             float f = float.Parse(s);
-            
 
+            var person = new Person();
+            person.firstName = "Arjun";
+            person.lastName = "Giridharan";
+            person.Introduce();
         }
     }
 }
